4ec484e4ec37fd4289114eb31b000eeea6a5afe9
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-12 12:18:14 -0700

    Added Service Worker

--- sw.js
+++ sw.js
@@ -1,21 +1,51 @@
-self.addEventListener('install', (event) => {
-  const urlsToCache = ['/', '/index.html', '/restaurant.html', '/css/styles.css', '/data/restaurants.json', '/img/1.jpg'];
+self.addEventListener("install", event => {
+  const urlsToCache = [
+    "/",
+    "/index.html",
+    "/restaurant.html",
+    "/css/styles.css",
+    "/data/restaurants.json",
+    "/img/1.jpg",
+    "/img/2.jpg",
+    "/img/3.jpg",
+    "/img/4.jpg",
+    "/img/5.jpg",
+    "/img/6.jpg",
+    "/img/7.jpg",
+    "/img/8.jpg",
+    "/img/9.jpg",
+    "/img/10.jpg",
+    "/js/dbhelper.js",
+    "/js/main.js",
+    "/js/restaurant_info.js"
+  ];
   event.waitUntil(
-    caches.open('restaurant-cache-v1').then((cache) => {
-      cache.addAll(urlsToCache)
+    caches.open("restaurant-cache-v1").then(cache => {
+      return cache.addAll(urlsToCache);
     })
-  )
-})
+  );
+});
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    fetch(event.request).then((response) => {
-      if (response.status == '404') {
-        return new Response("Not found")
+    caches.match(event.request).then((response) => {
+      if (response) {
+        console.log(`Found ${JSON.stringify(event.request)} in the cache.`)
+        return response;
+      } else {
+        console.log(`Didn't find ${JSON.stringify(event.request)} in the cache.`)
+        return fetch(event.request)
+          .then((response) => {
+            const responseClone = response.clone();
+            caches.open("v1").then((cache) => {
+              cache.put(event.request, responseClone)
+            });
+            return response;
+          })
+          .catch((err) => {
+            console.log(err);
+          })
       }
-      return response;
-    }).catch(() => {
-      return new Response("Bad boi")
     })
   )
-})
+});
