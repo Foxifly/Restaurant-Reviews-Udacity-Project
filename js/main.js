986b348a954b4b36aa4cc92d675a8475aa95838a
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-11 18:48:34 -0700

    Added gridcell attributes to restaurant elements.

--- js/main.js
+++ js/main.js
@@ -179,8 +179,12 @@ createRestaurantHTML = (restaurant) => {
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
-
+  addAttribute(li);
   return li
+}
+
+function addAttribute(li) {
+  li.setAttribute("role", "gridcell");
 }
 
 /**
