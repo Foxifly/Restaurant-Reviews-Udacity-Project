18aa6b50101eaeddf27d1a040e4e50af40320a23
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-12 11:24:09 -0700

    Beginning to cache urls with the service worker

--- sw.js
+++ sw.js
@@ -1,3 +1,21 @@
 self.addEventListener('install', (event) => {
-  console.log(event);
+  const urlsToCache = ['/', '/index.html', '/restaurant.html', '/css/styles.css', '/data/restaurants.json', '/img/1.jpg'];
+  event.waitUntil(
+    caches.open('restaurant-cache-v1').then((cache) => {
+      cache.addAll(urlsToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (response.status == '404') {
+        return new Response("Not found")
+      }
+      return response;
+    }).catch(() => {
+      return new Response("Bad boi")
+    })
+  )
 })
