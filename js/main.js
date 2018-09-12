6ee039a40894ade87b3959b756f2893fbf717f83
Lindsay Ciastko <LindsayCiastko@gmail.com>
2018-09-11 19:04:25 -0700

    Added aria labels to the restaurant info grid.

--- js/main.js
+++ js/main.js
@@ -160,24 +160,32 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.alt = `Image of restaurant: ${restaurant.name}`;
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h1');
   name.innerHTML = restaurant.name;
+  name.id = "restaurant-name";
+  name.setAttribute("aria-describedby", "restaurant-address");
   li.append(name);
 
   const neighborhood = document.createElement('p');
   neighborhood.innerHTML = restaurant.neighborhood;
+  neighborhood.setAttribute("aria-labelledby", "restaurant-name");
+  neighborhood.setAttribute("role", "note");
   li.append(neighborhood);
 
   const address = document.createElement('p');
   address.innerHTML = restaurant.address;
+  address.setAttribute("aria-labelledby", "restaurant-name");
+  address.setAttribute("role", "contentinfo");
   li.append(address);
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute("aria-labelledby", "restaurant-name");
   li.append(more)
   addAttribute(li);
   return li
